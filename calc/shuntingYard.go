a30d87a56d58634c3698ad97a147161961f1fe50
Rafael <rafael.julio.dev@outlook.com>
2025-05-31 20:57:28 -0300

    Continuando desenvolvimento
    - Detecção de pontos problemas, onde não ta funcionando corretamente

--- calc/shuntingYard.go
+++ calc/shuntingYard.go
@@ -2,6 +2,8 @@ package calculator
 
 import (
 	"errors"
+	"fmt"
+	"slices"
 	"strings"
 )
 
@@ -14,50 +16,76 @@ func ShuntingYard(expression string) ([]string, error) {
 
 	for _, s := range expression {
 
+		fmt.Println("[s]: ", string(s))
+		fmt.Println()
+		fmt.Println("operandStack: ", operandStack)
+		fmt.Println("operatorStack: ", operatorStack)
+
 		if strings.Contains(numericRange, string(s)) {
 			operandStack = append(operandStack, string(s))
 			continue
 		}
 
-		currentPrecedence, currentAssociativity, err := getPrecedenceInfo(s)
-
-		lastIndex := len(operandStack) - 1
-
-		lastPrecedence, lastAssociativity, _ := getPrecedenceInfo(rune(operandStack[lastIndex][0]))
+		currentPrecedence, currentAssociativity, err := getPrecedenceInfo(string(s))
 
 		if err != nil {
 			return []string{""}, err
 		}
 
+		if len(operatorStack) == 0 {
+			operatorStack = append(operatorStack, string(s))
+			continue
+		}
+
+		lastIndex := len(operatorStack) - 1
+
+		lastPrecedence, lastAssociativity, _ := getPrecedenceInfo(operatorStack[lastIndex])
+
 		if currentPrecedence > lastPrecedence || (currentPrecedence == lastPrecedence && currentAssociativity == lastAssociativity && currentAssociativity == "right") {
+
 			operatorStack = append(operatorStack, string(s))
 			continue
 		}
 
 		if currentPrecedence < lastPrecedence || (currentPrecedence == lastPrecedence && currentAssociativity == lastAssociativity && currentAssociativity == "left") {
+			// TODO O erro ta aqui
+			// Isso aqui precisa ter uma verificação pra tirar todo mundo que não bater a precedência corretamente!
+
 			value := operatorStack[len(operatorStack)-1]
 			operatorStack[len(operatorStack)-1] = ""
 			operatorStack = operatorStack[:len(operatorStack)-1]
 			operandStack = append(operandStack, value)
+			operatorStack = append(operatorStack, string(s))
+
 			continue
 		}
 
 	}
 
+	// Certo, aqui eu preciso adicionar todos os outros que restaram
+
+	operatorStack = slices.DeleteFunc(operatorStack, func(s string) bool {
+		return strings.TrimSpace(s) == ""
+	})
+
+	if len(operatorStack) >= 1 {
+		operandStack = append(operandStack, operatorStack...)
+	}
+
 	return operandStack, nil
 }
 
-func getPrecedenceInfo(r rune) (int, string, error) {
+func getPrecedenceInfo(r string) (int, string, error) {
 	switch r {
-	case '^':
+	case "^":
 		return 4, "right", nil
-	case '*':
+	case "*":
 		return 3, "left", nil
-	case '/':
+	case "/":
 		return 3, "left", nil
-	case '+':
+	case "+":
 		return 2, "left", nil
-	case '-':
+	case "-":
 		return 2, "left", nil
 	default:
 		return 0, "", errors.New("invalid operator informed")
